adf53bea1d850d31fc7cb14d23c85c28fef5f035
maja193 <99573533+maja193@users.noreply.github.com>
2024-04-02 16:46:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     if opt.config is not None:
         with open(opt.config, 'r') as ymlfile:
-            config = yaml.load(ymlfile)
+            config = yaml.safe_load(ymlfile)
     else:
         config = None
     main(opt, config)
